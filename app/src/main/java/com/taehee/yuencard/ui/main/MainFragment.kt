a8dc25adef8f2bf28e53407413e0e6ff9600ddcd
Ryu Tae Hee <ice1250@gmail.com>
2021-10-01 16:57:37 +0900

    - 앱 이름 변경 - 파티클 추가 - 코드 정리

--- app/src/main/java/com/taehee/yuencard/ui/main/MainFragment.kt
+++ app/src/main/java/com/taehee/yuencard/ui/main/MainFragment.kt
@@ -1,10 +1,12 @@
 package com.taehee.yuencard.ui.main
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -12,12 +14,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.taehee.yuencard.R
 import com.taehee.yuencard.databinding.MainFragmentBinding
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 import java.util.*
 
 class MainFragment : Fragment(), MainHandler, TextToSpeech.OnInitListener {
 
-    lateinit var tts: TextToSpeech;
-    lateinit var binding: MainFragmentBinding
+    private lateinit var tts: TextToSpeech
+    private lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -35,6 +39,7 @@ class MainFragment : Fragment(), MainHandler, TextToSpeech.OnInitListener {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
@@ -43,9 +48,81 @@ class MainFragment : Fragment(), MainHandler, TextToSpeech.OnInitListener {
 
         viewModel.getColor().observe(viewLifecycleOwner,
             { binding.main.setBackgroundColor(Color.parseColor(it)) })
+
+        binding.root.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    binding.particle.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(100L)
+                        .addShapes(Shape.Square, Shape.Circle)
+                        .addSizes(Size(14))
+                        .setPosition(motionEvent.x,
+                            motionEvent.x,
+                            motionEvent.y,
+                            motionEvent.y)
+                        .streamFor(100, 500L)
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    binding.particle.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(100L)
+                        .addShapes(Shape.Square, Shape.Circle)
+                        .addSizes(Size(14))
+                        .setPosition(motionEvent.x,
+                            motionEvent.x,
+                            motionEvent.y,
+                            motionEvent.y)
+                        .streamFor(10, 500L)
+                }
+            }
+            false
+        }
+        binding.cardView.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    binding.particle.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(100L)
+                        .addShapes(Shape.Square, Shape.Circle)
+                        .addSizes(Size(14))
+                        .setPosition(motionEvent.x + binding.cardView.x,
+                            motionEvent.x + binding.cardView.x,
+                            motionEvent.y + binding.cardView.y,
+                            motionEvent.y + binding.cardView.y)
+                        .streamFor(100, 500L)
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    binding.particle.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(100L)
+                        .addShapes(Shape.Square, Shape.Circle)
+                        .addSizes(Size(14))
+                        .setPosition(motionEvent.x + binding.cardView.x,
+                            motionEvent.x + binding.cardView.x,
+                            motionEvent.y + binding.cardView.y,
+                            motionEvent.y + binding.cardView.y)
+                        .streamFor(10, 500L)
+                }
+            }
+            false
+        }
     }
 
     override fun onClickCard(wordText: String) {
+
         tts.speak(wordText, TextToSpeech.QUEUE_FLUSH, null, "")
         viewModel.refresh()
     }
@@ -64,4 +141,6 @@ class MainFragment : Fragment(), MainHandler, TextToSpeech.OnInitListener {
         tts.shutdown()
         super.onDestroy()
     }
+
+
 }
