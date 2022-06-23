bf90ee4f316b1eddbcc5d677cb49870ed5e91c41
TaeheeRyu <taehee@yanadoocorp.com>
2022-06-23 16:28:00 +0900

    DeviceModule.kt 초기화 수정

--- presentation/src/main/java/com/taehee/wordcard/di/DeviceModule.kt
+++ presentation/src/main/java/com/taehee/wordcard/di/DeviceModule.kt
@@ -2,7 +2,6 @@ package com.taehee.wordcard.di
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,15 +13,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DeviceModule {
-    lateinit var textToSpeech: TextToSpeech
 
     @Provides
     @Singleton
     fun provideTextToSpeak(@ApplicationContext appContext: Context): TextToSpeech {
-        Log.i("taehee", "provideTextToSpeak")
-        textToSpeech = TextToSpeech(appContext) {
-            textToSpeech.language = Locale.KOREAN
-        }
+        lateinit var textToSpeech: TextToSpeech
+        textToSpeech = TextToSpeech(appContext) { textToSpeech.language = Locale.KOREAN }
         return textToSpeech
+
     }
 }
