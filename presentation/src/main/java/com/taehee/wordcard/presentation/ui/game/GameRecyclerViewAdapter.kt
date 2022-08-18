24007a4e151873f49ba8fbdaa1fdfef0f93fa612
TaeheeRyu <ice1250@gmail.com>
2022-08-18 17:40:18 +0900

    게임 레이아웃 수정

--- presentation/src/main/java/com/taehee/wordcard/presentation/ui/game/GameRecyclerViewAdapter.kt
+++ presentation/src/main/java/com/taehee/wordcard/presentation/ui/game/GameRecyclerViewAdapter.kt
@@ -1,10 +1,12 @@
 package com.taehee.wordcard.presentation.ui.game
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.view.View
 import androidx.core.content.ContextCompat
-import com.taehee.wordcard.domain.model.Game
 import com.taehee.presentation.R
 import com.taehee.presentation.databinding.ViewholderGameBinding
+import com.taehee.wordcard.domain.model.Game
 import com.taehee.wordcard.presentation.ui.custom.CustomAdapter
 import com.taehee.wordcard.presentation.ui.custom.DiffCallback
 
@@ -13,22 +15,36 @@ class GameRecyclerViewAdapter(
 ) : CustomAdapter<Game, ViewholderGameBinding>(
     R.layout.viewholder_game, DiffCallback.game(),
     bind = { item, binding ->
-        if (item.state == Game.GameState.NONE) {
-            binding.textView.visibility = View.GONE
-        } else {
-            binding.textView.visibility = View.VISIBLE
-        }
-        binding.textView.text = item.name
 
-        binding.cardView.setOnClickListener {
-            onClick(item)
+        binding.textView.apply {
+            text = item.name
+            visibility = if (item.state == Game.GameState.NONE) View.GONE else View.VISIBLE
         }
-        if (item.state == Game.GameState.SUCCESS) {
-            binding.cardView.setCardBackgroundColor(ContextCompat.getColor(binding.cardView.context,
-                R.color.shrine_pink_100))
-        } else {
-            binding.cardView.setCardBackgroundColor(ContextCompat.getColor(binding.cardView.context,
-                R.color.shrine_pink_light))
+
+        binding.cardView.apply {
+            setOnClickListener { onClick(item) }
+            setCardBackgroundColor(
+                ContextCompat.getColor(
+                    binding.cardView.context,
+                    if (item.state == Game.GameState.SUCCESS)
+                        R.color.shrine_pink_100
+                    else
+                        R.color.shrine_pink_light
+                )
+            )
+        }
+        binding.cardView.cameraDistance =
+            20000 * binding.root.context.resources.displayMetrics.density
+
+        if (item.state == Game.GameState.FLIP) {
+            val animatorSet = AnimatorInflater.loadAnimator(binding.root.context,
+                R.animator.game_animation) as AnimatorSet
+            val textAnimatorSet = AnimatorInflater.loadAnimator(binding.root.context,
+                R.animator.game_text_animation) as AnimatorSet
+            animatorSet.setTarget(binding.cardView)
+            textAnimatorSet.setTarget(binding.textView)
+            animatorSet.start()
+            textAnimatorSet.start()
         }
     }
 )
