a5ac2d45f6da7c8b571a299b26f914a9e564777d
Taehee Ryu <ice1250@gmail.com>
2023-02-07 11:27:32 +0900

    빌드오류 수정

--- presentation-compose/src/main/java/com/taehee/wordcard/presentation/ui/MainActivity.kt
+++ presentation-compose/src/main/java/com/taehee/wordcard/presentation/ui/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colors.background) {
+                color = MaterialTheme.colorScheme.background) {
                 Greeting("Android")
             }
         }
